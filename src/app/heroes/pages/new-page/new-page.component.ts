d760ff11c5fb0ac478ed37967ea3c663c364321a
enjima7 <enjima07@gmail.com>
2024-03-08 01:21:20 -0600

    add and edit hero done

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
@@ -6,10 +11,67 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+public heroForm = new FormGroup({
+  id: new FormControl<string>(''),
+  superhero: new FormControl<string>('', {nonNullable: true}),
+  publisher: new FormControl<Publisher>(Publisher.DCComics),
+  alter_ego: new FormControl(''),
+  first_appearance: new FormControl(''),
+  characters: new FormControl(''),
+  alt_img: new FormControl(''),
+})
 
   public publishers = [
     {id:'DC Comics', desc: 'DC - Comics'},
     {id:'Marvel Comics', desc: 'Marvel - Comics'}
-  ]
+  ];
+
+  constructor(private heroesService:HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router){}
+
+
+  ngOnInit(): void {
+
+    if(!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroById(id)),
+    ).subscribe(hero => {
+
+      if(!hero) return this.router.navigateByUrl('/');
+
+      this.heroForm.reset(hero);
+      return;
+    })
+  }
+
+  get currentHero ():Hero{
+    const hero = this.heroForm.value as Hero
+    return hero;
+  }
+
+
+  onSubmit():void{
+
+    if(this.heroForm.invalid) return;
+
+    if (this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+      .subscribe( hero => {
+
+      });
+
+      return;
+    }
+   // this.heroesService.updateHero();
+
+   this.heroesService.addHero(this.currentHero)
+   .subscribe( hero =>{
+
+   })
+  }
 }
